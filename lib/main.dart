13d95ed4cd79483f194d6173034278e54e47e8c2
alexgmuriithi <alex.gibson.muriithi@tutanota.com>
2020-04-14 16:25:55 +0300

    Alex Muriithi Flutter V-Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 60.0,
+                backgroundImage: AssetImage('images/ProfilePhoto.jpg'),
+              ), //Profile Picture
+              Text(
+                'Alex Gibson Muriithi',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ), // Name
+              Text(
+                'Software Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ), // Job Title
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.lightBlue.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.lightBlue,
+                  ),
+                  title: Text(
+                    '+254 725 777 153',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.lightBlue,
+                  ),
+                  title: Text(
+                    'alex.gibson.muriithi@gmail.com',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
